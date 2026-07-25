512613879d309ccc4109302aa891e83293a0c3e4
yusdesign <swiftcase@gmail.com>
2026-07-25 18:24:03 +0800

    Vitalk Free - Phase 1

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/index.html'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
